9708d9cc386e6b8792009cd0afea902289cfec70
Maciej Kubas <maciekkubas@gmail.com>
2018-05-06 01:38:53 +0200

    tasks-completed

---
+++ Zadanie 15.1 Ćwiczymy ES6/script-compiled.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// TASK 1
+
+var firstWord = 'Hello';
+var secondWord = 'World';
+
+var helloWorld = firstWord + ' ' + seondWord;
+
+console.log(helloWorld);
+
+//or
+
+var name = function name() {
+  var firstName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Maciej';
+  var secondName = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'Kubas';
+  return console.log(firstName + ' ' + secondName);
+};
+
+//TASK 2
