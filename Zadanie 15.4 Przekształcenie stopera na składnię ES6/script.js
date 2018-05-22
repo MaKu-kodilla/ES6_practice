3df7e7f22214a4eeed7c0f80bc8e0fa07437bf48
Maciej Kubas <maciekkubas@gmail.com>
2018-05-22 20:56:44 +0200

    questions

---
+++ Zadanie 15.4 Przekształcenie stopera na składnię ES6/script.js
@@ -0,0 +1,105 @@
+
+class App extends React.Component {
+    render() {
+    return (
+        <div className = {'App'}>
+        <Stopwatch />
+        </div>
+        )
+    }
+};
+
+class StopWatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            running: false,
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0,
+            },
+        };
+        this.handleStartClick = this.handleStartClick.bind(this);
+        this.handleStopClick = this.handleStopClick.bind(this);
+        this.handleResetClick = this.handleResetClick.bind(this);
+    }
+
+    reset () {
+        this.setState ({
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        });
+        this.print();
+    }
+    print () {
+        this.props.innerText = this.format();
+    }
+
+    format () {
+        let minutes = this.state.times.minutes;
+        let seconds = this.state.times.seconds;
+        let miliseconds = this.state.times.miliseconds;
+        return `${this.pad0(minutes)}:${this.pad0(seconds)}:${this.pad0(Math.floor(miliseconds))}`
+    }
+
+    start () {
+        if (!this.state.running) {
+            this.state.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+
+    step () {
+        if (!this.state.running) return;
+        this.calculate();
+        // this.print();
+    }
+
+    calculate () {
+        this.times.miliseconds +=1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds +=1
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+
+    stop () {
+        this.setState({
+            running: false
+    });
+
+    clearInterval(this.watch);
+}
+
+pad0 (value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+        return result;
+    }
+}
+
+
+render() {
+      return (
+        <div className = {'main'}>
+            <nav className = {'controls'}>
+                <a href = {'#'} className = {'button'} id = {'start'} onClick = {this.handleStartClick}>start</a>
+                <a href = {'#'} className = {'button'} id = {'stop'} onClick = {this.handleStopClick}>stop</a>
+            </nav>
+            <nav className={'controls'}>
+                <a href = {'#'} className = {'button'} id = {'reset'} onClick = {this.handleResetClick}>reset</a>
+            </nav>
+        </div>
+        )
+    }
+}
+
+const app = <App />;
+ReactDOM.render(app, document.getElementById('app'));
